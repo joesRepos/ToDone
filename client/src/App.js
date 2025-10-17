8e308b9a0f84c2b27cefed67edb0612d48231e9f
joesRepos <joemitchell1717@gmail.com>
2025-10-17 16:52:26 +0100

    Form entry for new tasks. New tasks are sent to the server.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, {useEffect, useState} from 'react'
 function App() { 
 
   const [currentTasksData, setCurrentTasksData] = useState([{}])
+  const MAX_PRIORITY = 5;
 
   useEffect(() => {
     fetch("/api/get-current-tasks", {
@@ -28,11 +29,63 @@ function App() {
     return rows;
   }
 
+  function DisplayPriorities() {
+    let rows = [];
+    for (let i = 0; i < MAX_PRIORITY; i++) {
+      let priority = i + 1;
+      rows.push(<option values={priority}>{priority}</option>)
+    }
+    return rows;
+  }
+
+  function NewTaskBox() {
+    let rows = [];
+    rows.push(<div>
+        <p>Task:</p>
+        <input type="text" id = "NewTask" placeholder="Answer" required/>
+        <p>Priority:</p>
+        <select id="NewPriority" name="NewPriority">
+                <option value="">Select</option>
+                <DisplayPriorities/>
+      </select>
+      <p>Due Date:</p>
+      <input type="date" id="NewDue" required />
+      </div>
+    )
+    return rows;
+  }
+
+  function SaveNewTask() {
+    let newTaskData = [];
+    console.log("New Task Saved.")
+    newTaskData.push(document.getElementById("NewTask").value);
+    newTaskData.push(document.getElementById("NewPriority").value);
+    newTaskData.push(document.getElementById("NewDue").value);
+    console.log(newTaskData);
+
+    fetch("api/save-new-task", {
+      method:'POST',
+      body: JSON.stringify({data: newTaskData}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data === "VALID") {
+        window.location.reload();
+      }
+    });
+
+  }
+
   return (
     <div classname ="task-page">
       <h1>To Do List</h1>
       <h2>Current Tasks</h2>
-      <DisplayTasks/>
+      <h2>Add Task</h2>
+      <NewTaskBox/>
+      <button type="button" id="button" onClick={SaveNewTask}>Save</button>
       <h2>Completed Tasks</h2>
     </div>
   )
--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+app.use(express.json());
 
 app.get("/api/get-current-tasks", (req, res) => {
+    console.log("Sending tasks.");
     res.json({"tasks":["task1", "task2", "task3"]})
+})
+
+app.post("/api/save-new-task", (req, res) => {
+    //const data = req.body.data;
+    console.log("Task recieved: " + req.body.data);
+    res.json("VALID");
 })
 
 app.listen(5000,() => {console.log("Server started on port 5000")})
