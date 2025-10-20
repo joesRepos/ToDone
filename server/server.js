0be7d06ea1391573989a4c9dbfbd56fe045948f0
joesRepos <joemitchell1717@gmail.com>
2025-10-20 17:26:16 +0100

    Can now save new tasks into the database.

--- server/server.js
+++ server/server.js
@@ -7,19 +7,25 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.get("/api/get-current-tasks", (req, res) => {
+app.get("/api/get-current-tasks", async (req, res) => {
     console.log("Sending tasks.");
+    const db = await initDB();
+    const tasks = await db.all('SELECT * FROM tasks');
+    console.log(tasks);
     res.json({"tasks":["task1", "task2", "task3"]})
 });
 
-app.post("/api/save-new-task", (req, res) => {
+app.post("/api/save-new-task", async (req, res) => {
     const data = req.body.data;
+    const db = await initDB();
     console.log("Task recieved: " + req.body.data);
+    await db.run('INSERT INTO tasks (task) VALUES (?)', [data]);
     res.json("VALID");
 });
 
 app.post("/api/completed-task", (req, res) => {
     const data = req.body.data;
+    
     console.log("Task completed: " + req.body.data);
     res.json("VALID");
 });
